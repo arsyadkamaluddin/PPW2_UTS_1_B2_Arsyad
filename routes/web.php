3b05d570a5ec7d23d205eb1b4d4d78e73837339a
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-10-08 14:41:33 +0700

    [DONE] CRUD functionality for player management added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('players',PlayerController::class);
+Route::redirect('/',route("players.index"));
